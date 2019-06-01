5f8189c1b4fbf1d007d12fb6222090bb0c912da5
Aldhair <u201315853@upc.edu.pe>
2019-06-01 00:36:41 -0500

    Main

---
+++ Proyectos/PapuEngine/ScreenList.cpp
@@ -0,0 +1,64 @@
+#include "ScreenList.h"
+#include "IGameScreen.h"
+
+
+ScreenList::ScreenList(Game* _game):game(_game)
+{
+}
+
+void ScreenList::setScreen(int nextScreen)
+{
+	currentIndex = nextScreen;
+}
+
+void ScreenList::addScreen(IGameScreen* newScreen)
+{
+	//newScreen->screenIndex = screens.size();
+	//screens.push_back(newScreen);
+	//newScreen->build();
+	//newScreen->setParent(game);
+}
+
+void ScreenList::destroy() {
+	//for (size_t i = 0; i < screens.size(); i++)
+	//{
+	//	screens[i]->destroy();
+	//}
+
+	//currentIndex = -1;
+	//screens.resize(0);
+}
+
+ScreenList::~ScreenList()
+{
+	destroy();
+}
+
+IGameScreen* ScreenList::moveNext() {
+	//IGameScreen* currentScreen = getCurrent();
+	//if (currentScreen->getNextScreen() != -1) {
+	//	currentIndex = currentScreen->getNextScreen();
+	//}
+	//return getCurrent();
+	return nullptr;
+}
+
+IGameScreen* ScreenList::movePrevious() {
+	//IGameScreen* currentScreen = getCurrent();
+	//if (currentScreen->getNextScreen() != -1)
+	//{
+	//	currentIndex = currentScreen->getPreviousScreen();
+	//}
+	//return getCurrent();
+	return nullptr;
+}
+
+IGameScreen* ScreenList::getCurrent(){
+	//if (currentIndex == -1)
+	//{
+	//	return nullptr;
+	//}
+	//return screens[currentIndex];
+	//IGameScreen* temp;
+	return nullptr;
+}
---
+++ Proyectos/PapuEngine/ScreenList.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+
+class Game;
+class IGameScreen;
+
+class ScreenList
+{
+public:
+	ScreenList(Game* game);
+	~ScreenList();
+
+	IGameScreen* moveNext();
+	IGameScreen* movePrevious();
+	void setScreen(int nextScreen);
+	void addScreen(IGameScreen* _newScreen);
+	void destroy();
+
+	IGameScreen* getCurrent();
+protected:
+	std::vector<IGameScreen*> screens;
+	Game* game;
+	int currentIndex = 1;
+};
+
